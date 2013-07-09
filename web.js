e39b391e7cbb6d17891271f06946333a1a004fca
inaworldofideas <skexhi@hotmail.co.uk>
2013-07-09 18:03:11 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,11 +2,24 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', function(request, response) 
+{
+  var fs = require('fs');
+  var buffer = new buffer();
+    
+    response.send('Hello World 2!' {
+  console.log("Listening on " + port);
 });
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
