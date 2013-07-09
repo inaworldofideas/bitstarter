bfcd0a848b7cdba3e339324b57f379cdab4268e8
inaworldofideas <skexhi@hotmail.co.uk>
2013-07-09 18:41:07 +0000

    Update web.js

--- web.js
+++ web.js
@@ -4,9 +4,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer = new Buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
- });
+var content = new Buffer ();
+
+var content = fs.readFileSync('index.html', 'utf8');
+response.send(content.toString());
+
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
